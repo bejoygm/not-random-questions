5d3d4035404d4dc1d36fd959c1e58f8180635dec
Bejoy Mathew <bejoygm@gmail.com>
2018-01-02 20:02:16 +0530

    webpack setup to use react

--- frontend/dist/app.bundle.js
+++ frontend/dist/app.bundle.js
@@ -67,7 +67,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-console.log("hello");
+console.log("this");
 
 /***/ })
 /******/ ]);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,41 @@
-const path = require('path')
+const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Constant with our paths
 const paths = {
-    DIST: path.resolve(__dirname, 'dist'),
-    JS: path.resolve(__dirname, 'src'),
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
 }
 
-// Webpack Configurations
+// Webpack configuration
 module.exports = {
-    entry: path.join(paths.JS, 'app.js'),
-    output: {
-        path: paths.DIST,
-        filename: 'app.bundle.js',
-    },
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+  // index.html is used as a template in which it'll inject bundled app.
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  // Enable importing JS files without specifying their's extenstion
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 }
