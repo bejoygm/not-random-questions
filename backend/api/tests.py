3f16dd242eb9e1a533475734ff00a99e8637e7f2
Bejoy Mathew <bejoygm@gmail.com>
2018-01-04 18:09:59 +0530

    added test for create goal api

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -20,4 +20,19 @@ class ModelTestCase(TestCase):
         new_count = Goal.objects.count()
         self.assertNotEqual(old_count, new_count)
 
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
 
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.goal_data = {'name': 'Make Crane Oragami'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json"
+        )
+
+    def test_api_can_create_a_goal(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
