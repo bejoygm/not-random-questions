331c8ebde6f999310bf6b69e51fad36be624a0ac
Bejoy George Mathew <bejoygm@gmail.com>
2018-02-18 19:00:14 +0530

    added goal detailed view, updated gitignore

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,10 +3,17 @@ from .serializers import GoalSerializer
 from .models import Goal
 
 class CreateView(generics.ListCreateAPIView):
-    """This class defines the create behavior of our test api."""
+    """Creates Goals"""
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
 
     def perform_create(self, serializer):
         """Save the post data when creating a new Goal."""
         serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+	"""Handles individual goals.
+	TOOD: Add write rules for admins of goals only"""
+	lookup_field = 'id'
+	queryset = Goal.objects.all()
+	serializer_class = GoalSerializer
